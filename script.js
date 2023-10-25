d8f858a6304612b9f7abe70e8a3867c9654e0245
Twoja_Nazwa_Użytkownika <Twj_Adres_Email>
2023-10-25 21:32:05 +0200

    Change description: unpacked new files from online domain klementowski.pl

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-  alert("STRONA W BUDOWIE!\nProjekt strony    x\nStruktura HTML     x\nStylizacja CSS\nusprawnienia+backend\nkolejne podstrony\n");
-});
+let isMenuVisible = false;
+
+function mobileMenu() {
+  const nav = document.querySelector('nav');
+  const navLinks = nav.querySelectorAll('a');
+
+  if(isMenuVisible){;
+    nav.style.top = "-120%";
+    isMenuVisible = false;
+  }
+  else{
+    nav.style.top = "0";
+    isMenuVisible = true;
+  }
+}
+/*
+  if(isMenuVisible){;
+    navLinks.forEach(link => {
+      link.style.display = 'none';
+    });
+    nav.style.removeProperty('height');
+    isMenuVisible = false;
+  }
+  else{
+    navLinks.forEach(link => {
+      link.style.display = 'block';
+    });
+    nav.style.height = "100vh";
+    isMenuVisible = true;
+  }
+}
+*/
