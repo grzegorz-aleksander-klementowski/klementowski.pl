44074460df235f875df9551bbe31fd1c788e05a5
Twoja_Nazwa_Użytkownika <Twj_Adres_Email>
2023-10-25 18:30:11 +0200

    Actualization of changes

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+  alert("STRONA W BUDOWIE!\nProjekt strony    x\nStruktura HTML     x\nStylizacja CSS\nusprawnienia+backend\nkolejne podstrony\n");
+});
